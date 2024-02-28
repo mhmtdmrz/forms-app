c0bd5392959cb9b22b9f8d42ca7d7c8df019351b
mhmtdmrz <mhmtdmrz97@gmail.com>
2024-02-29 00:21:55 +0300

    Yeni ürün ekleme ve güncelleme sayfaları geliştirildi. Arama yapılabilmesi sağlandı.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,96 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using FormsApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FormsApp.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController()
     {
-        _logger = logger;
+
     }
 
-    public IActionResult Index()
+    [HttpGet]
+    public IActionResult Index(string searchString, string category)
     {
+        var products = Repository.Products;
+        if (!String.IsNullOrEmpty(searchString))
+        {
+            ViewBag.SearchString = searchString;
+            products = products.Where(p => p.Name!.ToLower().Contains(searchString.ToLower())).ToList();
+        }
+
+        if (!String.IsNullOrEmpty(category) && category != "0")
+        {
+            products = products.Where(p => p.CategoryId == int.Parse(category)).ToList();
+        }
+
+        // ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name", category);
+        var model = new ProductViewModel()
+        {
+            Products = products,
+            Categories = Repository.Categories,
+            SelectedCategory = category
+        };
+
+        return View(model);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        ViewBag.Categories = ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name"); ;
         return View();
     }
 
-    public IActionResult Privacy()
+    [HttpPost]
+    public async Task<IActionResult> Create(Product model, IFormFile imageFile)
     {
-        return View();
+        var allowedExtensions = new[] { ".jpg", ".jpeg", ".png" };
+        var extension = Path.GetExtension(imageFile.FileName); //abc.jpg --> jpg olarak alır.
+        var randomFileName = string.Format($"{Guid.NewGuid().ToString()}{extension}");
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img", randomFileName);
+
+        if (imageFile != null)
+        {
+            if (!allowedExtensions.Contains(extension))
+            {
+                ModelState.AddModelError("", "Geçerli resim formatı seçiniz.");
+            }
+        }
+
+        if (ModelState.IsValid)
+        {
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await imageFile!.CopyToAsync(stream);
+            }
+            model.Image = randomFileName;
+            model.ProductId = Repository.Products.Count + 1;
+            Repository.CreateProduct(model);
+            return RedirectToAction("Index");
+        }
+        ViewBag.Categories = ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
+        return View(model);
+
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    public IActionResult Edit(int? id)
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        if(id == null)
+        {
+            return NotFound();
+        }
+        var entity = Repository.Products.FirstOrDefault(x=>x.ProductId == id);
+        if(entity == null)
+        {
+            return NotFound();
+        }
+        ViewBag.Categories = ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
+
+        return View(entity);
     }
+
 }
